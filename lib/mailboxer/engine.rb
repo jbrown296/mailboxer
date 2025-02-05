9cf3f15121d5894992dfec31c67ff9a881692d4c
jbrown296 <jbrown296@users.noreply.github.com>
2025-02-05 11:41:10 -0500

    Update engine.rb

--- lib/mailboxer/engine.rb
+++ lib/mailboxer/engine.rb
@@ -6,6 +6,9 @@ end
 
 module Mailboxer
   class Engine < Rails::Engine
+    # Ensure Mailboxer is autoloaded properly in Rails 6+
+    config.autoload_paths << File.expand_path("../../app/models/mailboxer", __FILE__)
+
     initializer "mailboxer.models.messageable" do
       ActiveSupport.on_load(:active_record) do
         extend Mailboxer::Models::Messageable::ActiveRecordExtension
